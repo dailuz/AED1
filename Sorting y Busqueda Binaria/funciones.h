92be46ec8d00cb03fce92a9e48e0c579fa2e305e
Daiana Sittner <dsittner@dc.uba.ar>
2017-06-28 14:46:44 -0300

    Create funciones.h

---
+++ Sorting y Busqueda Binaria/funciones.h
@@ -0,0 +1,9 @@
+#include <vector>
+using namespace std;
+
+void swap (int &a, int &b);
+vector <int> insertionSort (vector <int> vec);
+vector <int> selectionSort (vector <int> vec);
+vector <int> bubbleSort (vector <int> vec);
+vector <int> countingSort (vector <int> vec);
+bool binarySearch (vector <int> vec, int e);
