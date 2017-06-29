0883b22fa9ab33af4285fb972f250fa35b353184
Daiana Sittner <dsittner@dc.uba.ar>
2017-06-29 20:36:47 -0300

    Update ejercicios.h

--- Guia 10/ejercicios.h
+++ Guia 10/ejercicios.h
@@ -1,2 +1,8 @@
 #include <vector>
 using namespace std;
+
+int determinante (vector <vector <int> > vec);
+vector <vector <int> > matrizTranspuesta (vector <vector <int> > mat);
+vector <vector <int> > productoEntreMatrices (vector <vector <int> > mat1, vector <vector <int> > mat2);
+vector <vector <int> > productoDeMatrizConSuTranspuesta (vector <vector <int> > mat);
+vector <vector <int> > promedio4celdasAdyacentes (vector <vector <int> > mat);
