9353a898a49360ef97fcf00e57b14ca5adcfd623
Daiana Sittner <dsittner@dc.uba.ar>
2017-06-29 20:37:03 -0300

    Update main.cpp

--- Guia 10/main.cpp
+++ Guia 10/main.cpp
@@ -6,6 +6,155 @@ using namespace std;
 
 int main (){
 
+	/*vector <vector <int> > mat1(4);
+	mat1 [0][0] = 5;
+	mat1 [0][1] = 3;
+	mat1 [0][2] = 7;
+	mat1 [0][3] = -1;
+	mat1 [1][0] = 0; // no se modifica
+	mat1 [1][1] = 6;
+	mat1 [1][2] = 3;
+	mat1 [1][3] = 9;
+	mat1 [2][0] = 0; // no se modifica
+	mat1 [2][1] = 0; // no se modifica
+	mat1 [2][2] = 22;
+	mat1 [2][3] = 65;
+	mat1 [3][0] = 0; // no se modifica
+	mat1 [3][1] = 0; // no se modifica
+	mat1 [3][2] = 0; // no se modifica
+	mat1 [3][3] = 79;*/
+
+	//#define mat1 {{5,3,7,-1}, {0,6,3,9}, {0,0,22,65}, {0,0,0,79}}
+
+	/*vector <vector <int> > mat1(4);
+	mat1[0].push_back(5);
+	mat1[0].push_back(3);
+	mat1[0].push_back(7);
+	mat1[0].push_back(-1);
+	mat1[1].push_back(0);
+	mat1[1].push_back(6);
+	mat1[1].push_back(3);
+	mat1[1].push_back(9);
+	mat1[2].push_back(0);
+	mat1[2].push_back(0);
+	mat1[2].push_back(22);
+	mat1[2].push_back(65);
+	mat1[3].push_back(0);
+	mat1[3].push_back(0);
+	mat1[3].push_back(0);
+	mat1[3].push_back(79);
+
+	//cout << determinante(mat1);*/
+
+	/*vector <vector <int> > mat2(4);
+	mat2[0].push_back(-2);
+	mat2[0].push_back(2);
+	mat2[0].push_back(4);
+	mat2[0].push_back(7);
+	mat2[1].push_back(22);
+	mat2[1].push_back(1);
+	mat2[1].push_back(8);
+	mat2[1].push_back(13);
+	mat2[2].push_back(11);
+	mat2[2].push_back(9);
+	mat2[2].push_back(3);
+	mat2[2].push_back(51);
+	mat2[3].push_back(-3);
+	mat2[3].push_back(20);
+	mat2[3].push_back(31);
+	mat2[3].push_back(5);*/
+
+
+	/*for (int i = 0; i < matrizTranspuesta(mat2).size(); i++){
+		for (int j = 0; j < matrizTranspuesta(mat2).size(); j++){
+			cout << matrizTranspuesta(mat2)[i][j] << ",";
+		}
+		cout << endl;
+	}*/
+
+
+	/*vector <vector <int> > mat3 (3);
+	mat3[0].push_back(2);
+	mat3[0].push_back(0);
+	mat3[0].push_back(1);
+	mat3[1].push_back(3);
+	mat3[1].push_back(0);
+	mat3[1].push_back(0);
+	mat3[2].push_back(5);
+	mat3[2].push_back(1);
+	mat3[2].push_back(1);
+
+	vector <vector <int> > mat4 (3);
+	mat4[0].push_back(1);
+	mat4[0].push_back(0);
+	mat4[0].push_back(1);
+	mat4[1].push_back(1);
+	mat4[1].push_back(2);
+	mat4[1].push_back(1);
+	mat4[2].push_back(1);
+	mat4[2].push_back(1);
+	mat4[2].push_back(0);*/
+
+	/*for (int i = 0; i < productoEntreMatrices(mat3,mat4).size(); i++){
+		for (int j = 0; j < productoEntreMatrices(mat3,mat4).size(); j++){
+			cout << productoEntreMatrices(mat3,mat4)[i][j] << ",";
+		}
+		cout << endl;
+	}*/
+
+	/*for (int i = 0; i < productoDeMatrizConSuTranspuesta(mat3).size(); i++){
+		for (int j = 0; j < productoDeMatrizConSuTranspuesta(mat3).size(); j++){
+			cout << productoDeMatrizConSuTranspuesta(mat3)[i][j] << ",";
+		}
+		cout << endl;
+	}*/
+
+	/*vector <vector <int> > mat5 (6);
+	mat5[0].push_back(1);
+	mat5[0].push_back(3);
+	mat5[0].push_back(5);
+	mat5[0].push_back(2);
+	mat5[0].push_back(10);
+	mat5[0].push_back(1);
+	mat5[1].push_back(13);
+	mat5[1].push_back(15);
+	mat5[1].push_back(0);
+	mat5[1].push_back(1);
+	mat5[1].push_back(3);
+	mat5[1].push_back(6);
+	mat5[2].push_back(7);
+	mat5[2].push_back(20);
+	mat5[2].push_back(30);
+	mat5[2].push_back(2);
+	mat5[2].push_back(8);
+	mat5[2].push_back(9);
+	mat5[3].push_back(10);
+	mat5[3].push_back(11);
+	mat5[3].push_back(7);
+	mat5[3].push_back(-3);
+	mat5[3].push_back(42);
+	mat5[3].push_back(3);
+	mat5[4].push_back(17);
+	mat5[4].push_back(1);
+	mat5[4].push_back(4);
+	mat5[4].push_back(0);
+	mat5[4].push_back(7);
+	mat5[4].push_back(2);
+	mat5[5].push_back(22);
+	mat5[5].push_back(7);
+	mat5[5].push_back(3);
+	mat5[5].push_back(5);
+	mat5[5].push_back(9);
+	mat5[5].push_back(0);
+
+	for (int i = 0; i < promedio4celdasAdyacentes(mat5).size(); i++){
+		for (int j = 0; j < promedio4celdasAdyacentes(mat5).size(); j++){
+			cout << promedio4celdasAdyacentes(mat5)[i][j] << ",";
+		}
+		cout << endl;
+	}*/
+
+
 
 
 	return 0;
